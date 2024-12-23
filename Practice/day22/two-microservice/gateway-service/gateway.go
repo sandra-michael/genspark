3e5e29559c4811493f7d66aa521cbf4e53d15f59
sandra-michael <sandymike7@gmail.com>
2024-12-22 23:40:32 -0500

    day 22 microservices

--- Practice/day22/two-microservice/gateway-service/gateway.go
+++ Practice/day22/two-microservice/gateway-service/gateway.go
@@ -111,6 +111,9 @@ func main() {
 	//"/" matches anything to route map
 	//"/abc" will only match "/abc" and not any other path in the route
 	http.HandleFunc("/", gatewayHandler)
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("I am working and healthy"))
+	})
 
 	panic(http.ListenAndServe(":80", nil))
 
